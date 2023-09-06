8d5f65d93b3f647093831a30b057edef8cd2b136
AlexanderDev2004 <alexgaming2120@gmail.com>
2023-09-06 13:20:17 +0700

    Javaku

---
+++ Latihan03.java
@@ -0,0 +1,13 @@
+/**
+ * Latihan03
+ */
+
+ public class Latihan03{
+
+
+    public static void main(String[] args){
+        System.out.println("Nama Alex");
+        System.out.println("Saya adalah mahasiswa baru jurusan Teknologi Informasi-Polinema");
+
+    }
+ }    
---
+++ Tugas03.java
@@ -0,0 +1,19 @@
+/*
+ * Tugas03 
+ */
+
+
+ public class Tugas03{
+
+
+        public static void main(String[] args){
+        System.out.println("*** Identitas Diri ***");
+        System.out.println("Nama    :ALEXANDER AGUNG RAYA");
+        System.out.println("Absen   :3");
+        System.out.println("Kelas   :TI_1F ");
+        System.out.println("Prodi   :D-4 TEKNIK INFORMATIKA ");
+        System.out.println("Jurusan :TEKNOLOGI INFORMASI ");
+        
+    }
+
+ }
